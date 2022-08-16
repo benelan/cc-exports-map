60e916ccd12043d5ab7626589323744d49818ba2
Ben Elan <benelan@users.noreply.github.com>
2022-08-15 18:47:36 -0700

    add working example that uses dynamic imports

--- src/routes/calcite.svelte
+++ src/routes/calcite.svelte
@@ -1,12 +1,28 @@
 <script>
   import { setAssetPath } from "@esri/calcite-components/dist/components";
   import "@esri/calcite-components/dist/components/calcite-button.js";
-  // import "@esri/calcite-components/dist/calcite/calcite.css";
-
+  import "@esri/calcite-components/dist/calcite/calcite.css";
   setAssetPath(
     "https://unpkg.com/@esri/calcite-components/dist/calcite/assets"
   );
 
+  /**
+   * Using dynamic imports is how we have been doing it
+   * Uncomment onMount block below and comment the imports above for a working example
+   * */
+
+
+  // import { onMount } from 'svelte';
+  // onMount(async () => {
+  //   import("@esri/calcite-components/dist/components").then(
+  //     ({ setAssetPath }) =>
+  //       setAssetPath(
+  //         "https://unpkg.com/@esri/calcite-components/dist/calcite/assets"
+  //       )
+  //   );
+  //   import("@esri/calcite-components/dist/calcite/calcite.css");
+  //   import("@esri/calcite-components/dist/components/calcite-button.js");
+  // });
 </script>
 
 <main>
